a2c19e05ca98595154de8c01a197815c949302e4
Tanya <keratilemampheko20@gmail.com>
2022-09-15 14:51:07 +0200

    Recording

---
+++ config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCWCRQuXgaNM7H3e7KFZySCH9ifUpD7IrM",
+  authDomain: "voice-recorder-no-login.firebaseapp.com",
+  projectId: "voice-recorder-no-login",
+  storageBucket: "voice-recorder-no-login.appspot.com",
+  messagingSenderId: "702396431969",
+  appId: "1:702396431969:web:1225c1985691d644dd7420"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
